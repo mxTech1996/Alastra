022694e885d9bff38f91bed47ed54873856ead25
mxTech1996 <168294592+mxTech1996@users.noreply.github.com>
2024-08-10 15:42:09 -0600

    Add src/data/index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 
-export const pageName = "Alastra";
+export const pageName = "Altrasta";
 export const primaryColor = "#000000";
 export const backgroundColor = "#FFFFFF";
 export const makeClearBackground = (color) => {
